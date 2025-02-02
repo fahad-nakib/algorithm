fefb97b34b75a4eab06bb3ee5e53446cfe4ff553
Md. Fahad Nakib <fahadnakib27@gmail.com>
2025-02-02 21:30:13 +0600

    Update sum_of_even_odd_from_list.py

--- Python/sum_of_even_odd_from_list.py
+++ Python/sum_of_even_odd_from_list.py
@@ -2,7 +2,7 @@ list = [2,2,1,9,0,2,1,4,9,5,4,3,6,8,6]
 evenSum = 0
 oddSum = 0
 for item in list :
-    if(item%2 == 0):
+    if(item % 2 == 0):
         evenSum += item
     else :
         oddSum += item
